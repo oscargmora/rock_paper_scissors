1c978cec1e0bef40c129e79705533cb4df66fd9a
Oscar Mora <oscgmora@gmail.com>
2022-11-26 22:28:23 -0500

    Add DOM Manipulation
    This commit adds DOM Manipulation to the preexisting rock paper scissors game. It also discontinues the use of console log, and alerts.

--- javascript.js
+++ javascript.js
@@ -1,49 +1,169 @@
 var getComputerChoice = [
-    'Computer Picked: Rock',
-    'Computer Picked: Paper',
-    'Computer Picked: Scissors',
+    'Computer Fighter: Rock',
+    'Computer Fighter: Paper',
+    'Computer Fighter: Scissors',
+    'Computer Fighter: Lizard',
+    'Computer Fighter: Spock'
 ]
 
 let playerScore = 0;
 let computerScore = 0;
 let playerSelection;
 
+const winLose = document.querySelector('.win-lose');
+const rocks = document.querySelector('#rock');
+const papers = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const lizards = document.querySelector('#lizard');
+const spocks = document.querySelector('#spock');
+const yourFighter = document.querySelector('#your-fighter');
+const computerFighter = document.querySelector('#computer-fighter');
+const playerScoreText = document.querySelector('#player-score');
+const computerScoreText = document.querySelector('#computer-score');
+const rules = document.querySelector('.rules');
+
+rocks.addEventListener('click', () => {
+    yourFighter.textContent = 'Your Fighter: Rock';
+    playerSelection = 'ROCK';
+    game();
+});
+
+papers.addEventListener('click', () => {
+    yourFighter.textContent = 'Your Fighter: Paper';
+    playerSelection = 'PAPER';
+    game();
+});
+
+scissors.addEventListener('click', () => {
+    yourFighter.textContent = 'Your Fighter: Scissors';
+    playerSelection = 'SCISSORS';
+    game();
+});
+
+lizards.addEventListener('click', () => {
+    yourFighter.textContent = 'Your Fighter: Lizard';
+    playerSelection = 'LIZARD';
+    game();
+});
+
+spocks.addEventListener('click', () => {
+    yourFighter.textContent = 'Your Fighter: Spock';
+    playerSelection = 'SPOCK';
+    game();
+});
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'ROCK') {
-        if (computerSelection === 'Computer Picked: Rock') {
-            return('Draw! \nPlayer Score: ' + playerScore + '\nComputer Score: ' + computerScore);
-        } else if (computerSelection === 'Computer Picked: Paper') {
+        if (computerSelection === 'Computer Fighter: Rock') {
+            rules.textContent = ("It's a tie! No Points for Anyone")
+            return('Draw!');
+        } else if (computerSelection === 'Computer Fighter: Paper') {
             ++computerScore;
-            return('You lose! Paper beats rock \nPlayer Score: ' + playerScore + '\nComputer Score: ' + computerScore);
-        } else if (computerSelection === 'Computer Picked: Scissors') {
+            rules.textContent = ('Paper covers Rock');
+            return('You lose!');
+        } else if (computerSelection === 'Computer Fighter: Scissors') {
             ++playerScore;
-            return('You win! Rock beats scissors \nPlayer Score: ' + playerScore + '\nComputer Score: ' + computerScore);
+            rules.textContent = ('Rock Crushes Scissors');
+            return('You win!');
+        } else if (computerSelection === 'Computer Fighter: Lizard') {
+            ++playerScore;
+            rules.textContent = ('Rock crushes Lizard');
+            return('You win!');
+        } else if (computerSelection === 'Computer Fighter: Spock') {
+            ++computerScore;
+            rules.textContent = ('Spock vaporizes Rock');
+            return('You lose!');
         };
     } else if (playerSelection === 'PAPER') {
-        if (computerSelection === 'Computer Picked: Rock') {
+        if (computerSelection === 'Computer Fighter: Rock') {
             ++playerScore;
-            return('You win! Paper beats rock \nPlayer Score: ' + playerScore + '\nComputer Score: ' + computerScore);
-        } else if (computerSelection === 'Computer Picked: Paper') {
-            return('Draw! \nPlayer Score: ' + playerScore + '\nComputer Score: ' + computerScore);
-        } else if (computerSelection === 'Computer Picked: Scissors') {
+            rules.textContent = ('Paper covers Rock');
+            return('You win!');
+        } else if (computerSelection === 'Computer Fighter: Paper') {
+            rules.textContent = ("It's a Tie! No Points for Anyone");
+            return('Draw!');
+        } else if (computerSelection === 'Computer Fighter: Scissors') {
             ++computerScore;
-            return('You lose! Scissors beat Paper \nPlayer Score: ' + playerScore + '\nComputer Score: ' + computerScore)
-        }  
+            rules.textContent = ('Scissors cuts Paper');
+            return('You lose!')
+        } else if (computerSelection === 'Computer Fighter: Lizard') {
+            ++computerScore;
+            rules.textContent = ('Lizard eats Paper');
+            return('You lose!');
+        } else if (computerSelection === 'Computer Fighter: Spock') {
+            ++playerScore;
+            rules.textContent = ('Paper disproves Spock');
+            return('You win!');
+        };
     } else if (playerSelection === 'SCISSORS') {
-        if (computerSelection === 'Computer Picked: Rock') {
+        if (computerSelection === 'Computer Fighter: Rock') {
             ++computerScore;
-            return('You lose! Rock beats scissors \nPlayer Score: ' + playerScore + '\nComputer Score: ' + computerScore);
-        } else if (computerSelection === 'Computer Picked: Paper') {
+            rules.textContent = ('Rock crushed scissors');
+            return('You lose!');
+        } else if (computerSelection === 'Computer Fighter: Paper') {
             ++playerScore;
-            return('You win! Scissors beats paper \nPlayer Score: ' + playerScore + '\nComputer Score: ' + computerScore);
-        } else if (computerSelection === 'Computer Picked: Scissors') {
-            return('Draw! \nPlayer Score: ' + playerScore + '\nComputer Score: ' + computerScore);
-        }
+            rules.textContent = ('Scissors cuts Paper');
+            return('You win!');
+        } else if (computerSelection === 'Computer Fighter: Scissors') {
+            rules.textContent = ("It's a Tie! No Points for Anyone");
+            return('Draw!');
+        } else if (computerSelection === 'Computer Fighter: Lizard') {
+            ++playerScore;
+            rules.textContent = ('Scissors decapitates Lizard');
+            return('You win!');
+        } else if (computerSelection === 'Computer Fighter: Spock') {
+            ++computerScore;
+            rules.textContent = ('Spock smashes Scissors');
+            return('You lose!');
+        };
+    } else if (playerSelection === 'LIZARD') {
+        if (computerSelection === 'Computer Fighter: Rock') {
+            ++computerScore;
+            rules.textContent = ('Rock Crushes Lizard');
+            return('You lose!');
+        } else if (computerSelection === 'Computer Fighter: Paper') {
+            ++playerScore;
+            rules.textContent = ('Lizard eats Paper');
+            return('You win!');
+        } else if (computerSelection === 'Computer Fighter: Scissors') {
+            ++computerScore;
+            rules.textContent = ('Scissors decapitates Lizard');
+            return('You lose!');
+        } else if (computerSelection === 'Computer Fighter: Lizard') {
+            rules.textContent = ("It's a Tie! No Points for Anyone");
+            return('Draw!');
+        } else if (computerSelection === 'Computer Fighter: Spock') {
+            ++playerScore;
+            rules.textContent = ('Lizard poisons Spock');
+            return('You win!');
+        };
+    } else if (playerSelection === 'SPOCK') {
+        if (computerSelection === 'Computer Fighter: Rock') {
+            ++playerScore;
+            rules.textContent = ('Spock vaporizes Rock');
+            return('You win!');
+        } else if (computerSelection === 'Computer Fighter: Paper') {
+            ++computerScore;
+            rules.textContent = ('Paper disproves Spock');
+            return('You lose!');
+        } else if (computerSelection === 'Computer Fighter: Scissors') {
+            ++playerScore;
+            rules.textContent = ('Spock Smashes Scissors');
+            return('You win!');
+        } else if (computerSelection === 'Computer Fighter: Lizard') {
+            ++computerScore;
+            rules.textContent = ('Lizard Poisons Spock');
+            return('You lose!');
+        } else if (computerSelection === 'Computer Fighter: Spock') {
+            rules.textContent = ("It's a Tie! No Points for Anyone");
+            return('Draw!');
+        };
     } else {
         ++computerScore;
-        return('learn how to spell bozo \nAUTOMATIC COMPUTER POINT \nPlayer Score: ' + playerScore + '\nComputer Score: ' + computerScore);
+        rules.textContent = ('AUTOMATIC COMPUTER POINT');
+        return('learn how to spell bozo');
     }
-}
+};
 
 function end() {
     if (playerScore > computerScore) {
@@ -53,23 +173,15 @@ function end() {
     } else {
         alert("Tie Game")
     }
-}
+};
 
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice[Math.floor(Math.random() * getComputerChoice.length)];
-        let selection = prompt('Choose Your Fighter!');
-        if (selection === null) {
-            return;
-        } else {
-            let playerSelection = selection.toUpperCase();
-            console.log(computerSelection);
-            playRound;
-            console.log(playRound(playerSelection, computerSelection)); 
-        }
-    }
+ function game() {
+    let computerSelection = getComputerChoice[Math.floor(Math.random() * getComputerChoice.length)];
+    computerFighter.textContent = `${computerSelection}`;
+    playRound;
+    winLose.textContent = (playRound(playerSelection, computerSelection));
+    playerScoreText.textContent = `Your Score: ${playerScore}`;
+    computerScoreText.textContent = `Computer Score: ${computerScore}`; 
     end();
-}
-
-game();
+};
